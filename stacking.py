7b50712bc7f28e752e9df1f33c2a1e0501248f81
Werner Chao <oahcrenrew@gmail.com>
2016-12-15 18:01:51 -0500

    trained xgboost, dealing with mlp separately later

--- stacking.py
+++ stacking.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sklearn.cross_validation import KFold, train_test_split
 
 import xgboost as xgb
+from xgboost import XGBRegressor
+
 
 # Dealing with xgboost data
 train_xg = pd.read_csv('train.csv')
@@ -13,6 +15,8 @@ train_xg['log_loss'] = np.log(train_xg['loss'])
 # Creating columns for features, and categorical features
 features_col = [x for x in train_xg.columns if x not in ['id', 'loss', 'log_loss']]
 cat_features_col = [x for x in train_xg.select_dtypes(include=['object']).columns if x not in ['id', 'loss', 'log_loss']]
+for c in range(len(cat_features_col)):
+    train_xg[cat_features_col[c]] = train_xg[cat_features_col[c]].astype('category').cat.codes
 
 train_xg_x = np.array(train_xg[features_col])
 train_xg_y = np.array(train_xg['log_loss'])
@@ -21,19 +25,27 @@ xg_x_train, xg_x_test, xg_y_train, xg_y_test  = train_test_split(train_xg_x, tra
 print "XGB Training set X: ", xg_x_train.shape, ". Y: ", xg_y_train.shape
 print "XGB Testing set X: ", xg_x_test.shape, ". Y: ", xg_y_test.shape
 
+# Training xgboost
+folds = KFold(len(xg_x_train), shuffle=False, n_folds=3)
+for k, (train_index, test_index) in enumerate(folds):
+    xtr = xg_x_train[train_index]
+    ytr = xg_y_train[train_index]
+    xtest = xg_x_train[test_index]
+    ytest = xg_y_train[test_index]
+    xgboosting = XGBRegressor(n_estimators=200, \
+                           learning_rate=0.07, \
+                           gamma=0.2, \
+                           max_depth=8, \
+                           min_child_weight=6, \
+                           colsample_bytree=0.6, \
+                           subsample=0.9)
+    xgboosting.fit(xtr, ytr)
+    # np.savetxt('ensemble/xgb_pred_fold_{}.txt'.format(k), np.exp(xgboosting.predict(xtest)))
+    # np.savetxt('ensemble/xgb_test_fold_{}.txt'.format(k), ytest)
 
-# Dealing with MLP data. Note we don't log the loss.
-train_mlp = pd.read_csv('train.csv')
 
-# Making columns for features
-cat_names = [i for i in train_mlp.columns if 'cat' in i]
-train_mlp = pd.get_dummies(data=train_mlp, columns=cat_names)
-features_mlp = [x for x in train_mlp.columns if x not in ['id','loss']]
 
-train_mlp_x = np.array(train_mlp[features_mlp])
-train_mlp_y = np.array(train_mlp['loss'])
 
-mlp_x_train, mlp_x_test, mlp_y_train, mlp_y_test = train_test_split(train_mlp_x, train_mlp_y, test_size=0.25, random_state=31337)
-print "MLP Training set X: ", mlp_x_train.shape, ". Y: ", mlp_y_train.shape
-print "MLP Testing set X: ", mlp_x_test.shape, ". Y: ", mlp_y_test.shape
+
+
 
