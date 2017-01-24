272f6f6863e8abdd1995b0b95dfed8073ef28dd9
Werner Chao <oahcrenrew@gmail.com>
2017-01-23 23:57:17 -0500

    fixed utility refactor code

--- predict/utilities/data_prep.py
+++ predict/utilities/data_prep.py
@@ -24,24 +24,25 @@ class data_prep(object):
 
 
     @staticmethod
-    def data_prep_log(train_data, target=True):
+    def data_prep_log(train_xg, target=True):
         ''' Data preprocessing to fill in missing values and log the target variable.
         Return cleaned features & target.'''
-
-        if target:
-            train_xg['log_loss'] = np.log(train_xg['loss'])
-            print 'train_xg shape:', train_xg.shape
-            train_xg_y = np.array(train_xg['log_loss'])
-        else:
-            train_xg_y = None
 
         # Creating columns for features, and categorical features
         cat_features_col = [x for x in train_xg.select_dtypes(include=['object']).columns[0:131]]
         for c in range(len(cat_features_col)):
             train_xg[cat_features_col[c]] = train_xg[cat_features_col[c]].astype('category').cat.codes
-
         features_col = [x for x in train_xg.columns[0:131]]
-        print 'features_col check 1: ', train_xg[features_col].shape
+        print 'Creating new features...'
         train_xg_x = np.array(train_xg[features_col])
-        
+
+        # If there is a target variable, log it.
+        if target:
+            print 'Logging the target variable...'
+            train_xg['log_loss'] = np.log(train_xg['loss'])
+            train_xg_y = np.array(train_xg['log_loss'])
+        else:
+            train_xg_y = None
+
+        return train_xg_x, train_xg_y
 
--- predict/xgb_allstate.py
+++ predict/xgb_allstate.py
@@ -6,38 +6,20 @@
 
 import numpy as np
 import pandas as pd
-
 import xgboost as xgb
 from xgboost import XGBRegressor
-
 from sklearn.model_selection import KFold
+from utilities import data_prep
 
 if __name__ == '__main__':
     # Preprocess data for xgboost.
     train_xg = pd.read_csv('../data/train.csv')
+    train_xg_x, train_xg_y = data_prep.data_prep_log(train_xg)
 
-    # Training xgboost. Out-of-fold prediction
-    folds = KFold(n_splits=3, shuffle=False)
-    for k, (train_index, test_index) in enumerate(folds.split(train_xg_x)):
-        xtr = train_xg_x[train_index]
-        ytr = train_xg_y[train_index]
-        xtest = train_xg_x[test_index]
-        ytest = train_xg_y[test_index]
-        print "xtest shape: ", xtest.shape
-        print "ytest shape: ", ytest.shape
-        xgboosting = XGBRegressor(n_estimators=200, \
-                            learning_rate=0.07, \
-                            gamma=0.2, \
-                            max_depth=8, \
-                            min_child_weight=6, \
-                            colsample_bytree=0.6, \
-                            subsample=0.9)
-        xgboosting.fit(xtr, ytr)
-        np.savetxt('xgb_pred_fold_{}.txt'.format(k), np.exp(xgboosting.predict(xtest)))
-        np.savetxt('xgb_test_fold_{}.txt'.format(k), ytest)
+    test_xg = pd.read_csv('../data/test.csv') #TODO: need to preprocess the data just like the train set.
+    test_xg_x, test_xg_y = data_prep.data_prep_log(test_xg, False)
 
-
-    # Training xgboost on the whole set.
+    # Training xgboost on CV set and predict using out-of-fold prediction
     xgboosting = XGBRegressor(n_estimators=200, \
                             learning_rate=0.07, \
                             gamma=0.2, \
@@ -45,6 +27,21 @@ if __name__ == '__main__':
                             min_child_weight=6, \
                             colsample_bytree=0.6, \
                             subsample=0.9)
+
+    folds = KFold(n_splits=3, shuffle=False)
+    for k, (train_index, test_index) in enumerate(folds.split(train_xg_x)):
+        xtr = train_xg_x[train_index]
+        ytr = train_xg_y[train_index]
+        xtest = train_xg_x[test_index]
+        ytest = train_xg_y[test_index]
+        print "Fitting on fold {}...".format(k)
+        print "Checking xtest shape: ", xtest.shape
+        print "Checking ytest shape: ", ytest.shape
+        xgboosting.fit(xtr, ytr)
+        np.savetxt('xgb_pred_fold_{}.txt'.format(k), np.exp(xgboosting.predict(xtest)))
+        np.savetxt('xgb_test_fold_{}.txt'.format(k), ytest)
+
+    # Training xgboost on test set.
     xgboosting.fit(train_xg_x, train_xg_y)
-    test_xg = pd.read_csv('../data/test.csv') #TODO: need to preprocess the data just like the train set.
-    np.savetxt('xgb_pred_test.txt', np.exp(xgboosting.predict(test_xg)))
+    print "Fitting on test set..."
+    np.savetxt('xgb_pred_test.txt', np.exp(xgboosting.predict(test_xg_x)))
