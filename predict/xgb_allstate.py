b60becbcf26dc8c83e887d63270e7d1bf744d146
Werner Chao <oahcrenrew@gmail.com>
2017-04-20 19:53:44 -0400

    added new params for xgboost

--- predict/xgb_allstate.py
+++ predict/xgb_allstate.py
@@ -20,13 +20,35 @@ if __name__ == '__main__':
     test_xg_x, test_xg_y = data_prep.data_prep_log(test_xg, False)
 
     # Training xgboost on CV set and predict using out-of-fold prediction
-    xgboosting = XGBRegressor(n_estimators=200, \
-                            learning_rate=0.07, \
-                            gamma=0.2, \
-                            max_depth=8, \
-                            min_child_weight=6, \
-                            colsample_bytree=0.6, \
-                            subsample=0.9)
+    xgboosting = XGBRegressor(n_estimators=5000, \
+                            learning_rate=0.05, \
+                            gamma=2, \
+                            max_depth=12, \
+                            min_child_weight=1, \
+                            colsample_bytree=0.5, \
+                            subsample=0.8, \
+                            reg_alpha=1, \
+                            objective='reg:linear', \
+                            base_score = 7.76)
+
+#res = xgb.cv(
+#           colsample_bytree = 0.5,
+#           subsample = 0.8,
+#           eta = 0.05, # replace this with 0.01 for local run to achieve 1113.93
+#           objective = 'reg:linear',
+#           max_depth = 12,
+#           alpha = 1,
+#           gamma = 2,
+#           min_child_weight = 1,
+#           base_score = 7.76
+#           nrounds=5000,
+#           nfold=5,
+#           early_stopping_rounds=15,
+#           print_every_n = 10,
+#           verbose= 1,
+#           feval=xg_eval_mae,
+#           maximize=FALSE
+#           )
 
     folds = KFold(n_splits=3, shuffle=False)
     for k, (train_index, test_index) in enumerate(folds.split(train_xg_x)):
@@ -37,11 +59,11 @@ if __name__ == '__main__':
         print "Fitting on fold {}...".format(k)
         print "Checking xtest shape: ", xtest.shape
         print "Checking ytest shape: ", ytest.shape
-        xgboosting.fit(xtr, ytr)
+        xgboosting.fit(xtr, ytr, verbose=True)
         np.savetxt('xgb_pred_fold_{}.txt'.format(k), np.exp(xgboosting.predict(xtest)))
         np.savetxt('xgb_test_fold_{}.txt'.format(k), ytest)
 
-    # Training xgboost on test set.
-    xgboosting.fit(train_xg_x, train_xg_y)
+    # Training xgboost on test set (i.e. whole train set).
+    xgboosting.fit(train_xg_x, train_xg_y, verbose=True)
     print "Fitting on test set..."
     np.savetxt('xgb_pred_test.txt', np.exp(xgboosting.predict(test_xg_x)))
