96c06d53284836923107c2691c62e00fd2639979
Werner Chao <oahcrenrew@gmail.com>
2017-04-30 14:09:32 -0400

    tried different learning rates

--- data/tensorflow_linear.py
+++ data/tensorflow_linear.py
@@ -3,41 +3,50 @@ from __future__ import print_function
 import tensorflow as tf
 import pandas as pd
 import numpy
+from collections import OrderedDict
 import matplotlib.pyplot as plt
 rng = numpy.random
 
 
 # Parameters
-learning_rate = 0.01
-training_epochs = 1000
-display_step = 50
+learning_rate = 1.2
+training_epochs = 10
+display_step = 1
 
 # Training Data
-# train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
-#                         7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-#train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
-#                         2.827,3.465,1.65,2.904,2.42,2.94,1.3])
-train_X = pd.read_csv('train.csv').iloc[0:141240, 117:131]
+train_X = pd.read_csv('train.csv').iloc[0:141240, 117:131] # Using only 14 efatures (117~131)
 train_Y = pd.read_csv('train.csv').iloc[0:141240, 131:132]
-
 train_X = numpy.array(train_X)
 train_Y = numpy.array(train_Y)
 
+# train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
+#                          7.042,10.791,5.313,7.997,5.654,9.27,3.1])
+# train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
+#                          2.827,3.465,1.65,2.904,2.42,2.94,1.3])
 n_samples = train_X.shape[0]
 
+# Cross Validation Data
+test_X = pd.read_csv('train.csv').iloc[141241:188318, 117:131] # Using only 14 efatures (117~131)
+test_Y = pd.read_csv('train.csv').iloc[141241:188318, 131:132]
+test_X = numpy.array(test_X)
+test_Y = numpy.array(test_Y)
+# test_X = numpy.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
+# test_Y = numpy.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03]
+
 # tf Graph Input
-X = tf.placeholder("float")
-Y = tf.placeholder("float")
+X = tf.placeholder(tf.float32, [None, 14])
+Y = tf.placeholder(tf.float32, [None, 1])
+
 
 # Set model weights
-W = tf.Variable(rng.randn(), name="weight")
-b = tf.Variable(rng.randn(), name="bias")
+W = tf.Variable(tf.zeros([14, 1]), name="weight")
+b = tf.Variable(tf.zeros([1]), name="bias")
 
 # Construct a linear model
-pred = tf.add(tf.multiply(X, W), b)
+pred = tf.add(tf.matmul(X, W), b)
 
 # Mean squared error
-cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
+cost = tf.reduce_sum(tf.abs(pred-Y))/(n_samples)
 # Gradient descent
 #  Note, minimize() knows to modify W and b because Variable objects are trainable=True by default
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
@@ -50,50 +59,54 @@ with tf.Session() as sess:
     sess.run(init)
 
     # Fit all training data
+    train_error = []
+    cv_error = []
+    count = 0
+    plt.ion()
     for epoch in range(training_epochs):
         for (x, y) in zip(train_X, train_Y):
+            x = numpy.reshape(x, (1, 14))
+            y = numpy.reshape(y, (1, 1))
             sess.run(optimizer, feed_dict={X: x, Y: y})
-	    c_1 = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
-	    print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c_1), \
-                "W=", sess.run(W), "b=", sess.run(b))
+	    # c_1 = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
+	    # print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c_1), \
+        #         "W=", sess.run(W), "b=", sess.run(b))
 
         # Display logs per epoch step
         if (epoch+1) % display_step == 0:
             c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
-            print("!!!Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
-                "W=", sess.run(W), "b=", sess.run(b))
+            print("!!!Epoch:", '%04d' % (epoch+1), "\n Training Cost=", "{:.9f}".format(c), \
+                "\nW=", sess.run(W), "\nb=", sess.run(b))
+
+            print("Testing... (Mean Absolute error Comparison)")
+            testing_cost = sess.run(
+                tf.reduce_sum(tf.abs(pred - Y)) / (test_X.shape[0]),
+                feed_dict={X: test_X, Y: test_Y})  # same function as cost above
+            print("Testing cost=", testing_cost)
+            print("Mean Absolute Error Difference:", (testing_cost - c))
+            train_error.append(c)
+            cv_error.append(testing_cost)
+            count = count + 1
+
+            # Graphic display
+            plt.plot(range(count), cv_error, '-ro', label='CV Error')
+            plt.plot(range(count), train_error, '-bo', label='Training Error')
+            handles, labels = plt.gca().get_legend_handles_labels()
+            by_label = OrderedDict(zip(labels, handles))
+            plt.legend(by_label.values(), by_label.keys())
+            plt.pause(0.01)
+
+    plt.ioff()
+    _ = plt.plot(range(count), cv_error, '-ro', label='CV Error')
+    _ = plt.plot(range(count), train_error, '-bo', label='Training Error')
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = OrderedDict(zip(labels, handles))
+    _ = plt.legend(by_label.values(), by_label.keys())
+    _ = plt.show()
 
     print("Optimization Finished!")
     training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
     print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
 
-    # Graphic display
-    # plt.plot(train_X, train_Y, 'ro', label='Original data')
-    # plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
-    # plt.legend()
-    # plt.show()
-
-    # Testing example, as requested (Issue #2)
-    # test_X = numpy.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
-    # test_Y = numpy.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03])
-
-    test_X = pd.read_csv('train.csv').iloc[141241:188318, 171:131]
-    test_Y = pd.read_csv('train.csv').iloc[141241:188318, 131:132]
-
-    test_X = numpy.array(test_X)
-    test_Y = numpyarray(test_Y)
-
-    print("Testing... (Mean square loss Comparison)")
-    testing_cost = sess.run(
-        tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * test_X.shape[0]),
-        feed_dict={X: test_X, Y: test_Y})  # same function as cost above
-    print("Testing cost=", testing_cost)
-    print("Absolute mean square loss difference:", abs(
-        training_cost - testing_cost))
-
-    # plt.plot(test_X, test_Y, 'bo', label='Testing data')
-    # plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
-    # plt.legend()
-    # plt.show()
 
 
